ac0735ee33947478cb99bdd62d7698d8bc1d5baf
Chatree-Puangpachung <jorjza1150>
2026-09-20 10:35:07 -0700

    update v4

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,26 @@
  
  
  // โหลด Menu.js
- fetch("Menu.html")
+ fetch("manu.html")
  .then(res => res.text())
  .then(data => {
      document.getElementById("menu-container").innerHTML = data;
+
+     // ✅ re-bind toggle หลังจากเมนูถูก inject
+     const sidebarToggle = document.body.querySelector('#sidebarToggle');
+     if (sidebarToggle) {
+         sidebarToggle.addEventListener('click', event => {
+             event.preventDefault();
+             document.body.classList.toggle('sb-sidenav-toggled');
+         });
+     }
+ });
+
+  // โหลด Menu.js
+ fetch("footers.html")
+ .then(res => res.text())
+ .then(data => {
+     document.getElementById("footers").innerHTML = data;
 
      // ✅ re-bind toggle หลังจากเมนูถูก inject
      const sidebarToggle = document.body.querySelector('#sidebarToggle');
